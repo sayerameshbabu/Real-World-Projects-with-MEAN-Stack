6743d99fa7aea792b0aa898282938072f4d8b20a
Ramesh babu saye <43689480+sayerameshbabu@users.noreply.github.com>
2020-12-14 08:13:22 +0530

    Update config.js

--- 06_05/server/config.js
+++ 06_05/server/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURL: 'mongodb://localhost:27017/learnit'
+  mongoURL: 'mongodb://ramesh:Yaram300595@docdb-2020-12-12-09-45-06.cluster-cfl84ckndzgl.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false/learnit'
 }
